8f30952cc6ce22e34a8583b2f5d44c9ba35a21c6
Adrianne Leung <adrianne.l.wk@gmail.com>
2022-01-13 16:54:47 -0800

    Added CleverStopwords.py

---
+++ src/clevercloud/CleverStopwords.py
@@ -0,0 +1,20 @@
+def CleverStopwords(words):
+    """
+    A comprehensive list of English stopwords that allow adding more customized words.    
+    
+    Parameters
+    ----------
+    words : set of strings 
+        Input a set of words that need to be included in the stopwords 
+    
+    Returns
+    -------
+    set of strings  
+        The complete set of stopwords 
+    
+    Examples
+    --------
+    >>> sample_words = {'disney', 'paris'}
+    >>> CleverStopwords(sample_words)
+​
+    """
